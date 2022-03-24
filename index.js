cf328d2cb9e2e49377c71e86432f325f5b86cc61
Aleksandr-Glebik <glebikaa@mail.ru>
2022-03-25 00:57:06 +0300

    feat: add func getNameBirds changeAudio

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 
 const audio = document.querySelector('.audio')
 const btnPlay = document.querySelector('.btn-play')
+const navList = document.querySelector('.nav-list')
+const objVoiceBirds = {
+    'solovey': './assets/audio/solovey.mp3',
+    'drozd': './assets/audio/drozd.mp3',
+    'zarynka': './assets/audio/zarynka.mp3',
+    'javoronok': './assets/audio/javoronok.mp3',
+    'slavka': './assets/audio/slavka.mp3',
+    'forest': './assets/audio/forest.mp3',
+}
+
 let isPlay = false
 
 btnPlay.addEventListener('click', playAudio)
+navList.addEventListener('click', getNameBirds)
 
 function playAudio() {
     if (!isPlay) {
@@ -14,5 +25,22 @@ function playAudio() {
         audio.pause()
         isPlay = false
     }
+}
+
+function getNameBirds(event) {
+    if (event.target.classList.contains('btn-birds')) {
+        let value = event.target.dataset.birds
+        // console.log('value', value);
+        changeAudio(value)
+    }
+}
+
+function changeAudio(value) {
+    let src = objVoiceBirds[value]
+    // console.log('src', src);
+    // console.log('src get', audio.getAttribute('src', src));
+    audio.setAttribute('src', src)
+    isPlay = false
+    playAudio()
 }
 
