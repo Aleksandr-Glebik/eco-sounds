f2a918341a5e0424254059d2a647d689ab453048
Aleksandr-Glebik <glebikaa@mail.ru>
2022-03-25 00:12:19 +0300

    feat: add func playAudio

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// console.log('hello');
+
+const audio = document.querySelector('.audio')
+const btnPlay = document.querySelector('.btn-play')
+let isPlay = false
+
+btnPlay.addEventListener('click', playAudio)
+
+function playAudio() {
+    if (!isPlay) {
+        audio.currentTime = 0
+        audio.play()
+        isPlay = true
+    } else if (isPlay) {
+        audio.pause()
+        isPlay = false
+    }
+}
+
