244ec8b3c62f4969f99760393b8297df631760f0
Aleksandr-Glebik <glebikaa@mail.ru>
2022-03-25 13:54:21 +0300

    fix: func addActiveClass and addEventListener on headerContainer

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const audio = document.querySelector('.audio')
 const btnPlay = document.querySelector('.btn-play')
 const btnPlayIcon = document.querySelector('.play > use')
-const navList = document.querySelector('.nav-list')
+const headerContainer = document.querySelector('.header-container')
 const mainContainer = document.querySelector('.main')
 const btnLogo = document.querySelector('.logo')
+const btnLogoUse = document.querySelector('.logo > use')
+const btnBirds = document.querySelectorAll('.btn-birds')
 const objVoiceBirds = {
     'solovey': './assets/audio/solovey.mp3',
     'drozd': './assets/audio/drozd.mp3',
@@ -25,7 +27,7 @@ const objImgBirds = {
 let isPlay = false
 
 btnPlay.addEventListener('click', playAudio)
-navList.addEventListener('click', getNameBirds)
+headerContainer.addEventListener('click', getNameBirds)
 btnLogo.addEventListener('click', returnInStart)
 
 function playAudio() {
@@ -44,9 +46,11 @@ function playAudio() {
 function getNameBirds(event) {
     if (event.target.classList.contains('btn-birds')) {
         let value = event.target.dataset.birds
-        // console.log('value', value);
         changeAudio(value)
         changeImg(value)
+        addActiveClass(event)
+    } else if (event.target == btnLogo || event.target == btnLogoUse) {
+        addActiveClass(event)
     }
 }
 
@@ -58,12 +62,10 @@ function changeAudio(value) {
     isPlay = false
     playAudio()
     changeIconPlayer(isPlay)
-
 }
 
 function changeIconPlayer(isPlay) {
     if (isPlay) {
-        // btnPlayIcon.setAttribute('xlink:href', '../eco-sounds/assets/svg/sprite.svg#pause')
         btnPlayIcon.setAttribute('xlink:href', './assets/svg/sprite.svg#pause')
     } else if (!isPlay) {
         btnPlayIcon.setAttribute('xlink:href', './assets/svg/sprite.svg#play')
@@ -81,6 +83,22 @@ function returnInStart() {
     audio.pause()
     mainContainer.style.backgroundImage = `url(${'./assets/img/forest.jpg'})`
     audio.setAttribute('src', './assets/audio/forest.mp3')
-    btnPlayIcon.setAttribute('xlink:href', '../eco-sounds/assets/svg/sprite.svg#play')
+    btnPlayIcon.setAttribute('xlink:href', './assets/svg/sprite.svg#play')
+    btnLogo.classList.add('active')
+}
+
+function addActiveClass(event) {
+    if (event.target.classList.contains('btn-birds')) {
+         btnBirds.forEach((el) => {
+            el.classList.remove('active')
+        })
+        btnLogo.classList.remove('active')
+        event.target.classList.add('active')
+    }
+    if (event.target.classList.contains('logo') || event.target == btnLogoUse) {
+        btnBirds.forEach((el) => {
+            el.classList.remove('active')
+        })
+    }
 }
 
